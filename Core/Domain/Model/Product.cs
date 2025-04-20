6203dc168c5c36a00235904fb357942245a87293
Greg Low <greg@gmail.com>
2025-04-20 17:14:33 +0200

    Add project files.

---
+++ Core/Domain/Model/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Model
+{
+    public class Product : BaseEntity<int>
+    {
+     public string Name { get; set; }
+
+     public string Description { get; set; }
+
+     public string PictureUrl { get; set; }
+
+     public decimal Price { get; set; }
+
+     public int BrandId { get; set; } //fk
+
+     public ProductBrand ProductBrand { get; set; } //Navigational property
+    
+     public int TypeId { get; set; } //fk
+     public ProductType ProductType { get; set; } //Navigational property
+    }
+}
---
+++ Infrastructure/Persistence/Data/StoreDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Data
+{
+    public class StoreDbContext :DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+
+        public DbSet<ProductType> ProductTypes { get; set; }
+    }
+}
